b0a91b5aa800b1c51e0d30822b75f10cdb8d148d
diegoNguizzo <dguizzo@gmail.com>
2022-10-23 17:56:24 -0300

    Reorganizacion de carpetas

--- SP-Funciones-Triggers-Errores-PL_SQL/SQLQuery_Unidad2.sql
+++ SP-Funciones-Triggers-Errores-PL_SQL/SQLQuery_Unidad2.sql
@@ -2,24 +2,7 @@ use libreriaLCII
 go
 
 
--- Try.....Catch
 
-begin try 
-select 15/0 as error;
-end try
-begin catch
-select 'se produjo el siguiente error',
-error_number () as Número,
-error_state () as Estado,
-error_severity () as Gravedad,
-error_procedure () as Procedimiento,
-error_line () as Linea,
-error_message () as Mensaje
-end catch
-
-
-select * from sys.messages
-where message_id = 8134
 
 
 ----------------------------------------------------------------------------------------------------------------
@@ -53,12 +36,12 @@ set @stock = (select stock from articulos where cod_articulo = @codigo)
 set @stockMinimo = (select stock_minimo from articulos where cod_articulo = @codigo)
 if (@stock < @stockMinimo)
 begin
-select 'Es necesario reponer'
+select 'Es necesario reponer'+ trim(str(@codigo))
 return 1
 end;
 else
 begin
-select 'No es necesario reponer'
+select 'No es necesario reponer'+ trim(str(@codigo))
 return 0 
 end;
 
@@ -111,7 +94,7 @@ En caso de ser ambos números iguales mostrar un mensaje informando dicha situaci
 select * from sys.objects where type = 'fn'
 
 
-CREATE function fn_suma
+ALTER function fn_suma
 (@numero1 int, @numero2 int)
     returns varchar(50)
     as
@@ -122,8 +105,11 @@ CREATE function fn_suma
 
         if(@numero1 = @numero2)
             return 'Los numeros son iguales'
-       
-        SET @numero = @numero1
+        else
+        if (@numero1>=@numero2)
+	        return 'n1 debe ser menor que n2'   
+        else
+		SET @numero = @numero1
         while (@numero <= @numero2)
         begin
             set @suma += @numero
@@ -132,10 +118,7 @@ CREATE function fn_suma
         return CAST(@suma as varchar)
     end
 
-select dbo.fn_suma (5,10)
-
-
--------------------------------------------------------------------------
+select dbo.fn_suma (15,10)
 
 
 
@@ -166,28 +149,106 @@ from articulos
 
 /*Ejercicio 1.6.  Modificar el punto 2 reemplazando el mensaje de que es necesario reponer artículos por una excepción.*/
 
-alter proc pa_consulta_repo_stock2
+declare @codigo int=30, @stock int, @stockMinimo int
+select @stock=stock ,@stockminimo=stock_minimo
+	from articulos
+	where cod_articulo=@codigo
+if @stock is null or @stockMinimo is null 
+	raiserror ('hay valores nulos',11,1)
+else
+	if @stock<=@stockMinimo
+		raiserror ('realizar la reposición del stock',10,1)
+	else
+		raiserror ('No es necesario realizar la reposición del stock',10,1)
+
+
+
+
+alter proc pa_consulta_repo_stock3
 @codigo int = 3,
 @stock decimal (10,2) output,
 @stockMinimo decimal (10,2) output
 as
 set @stock = (select stock from articulos where cod_articulo = @codigo)
 set @stockMinimo = (select stock_minimo from articulos where cod_articulo = @codigo)
-try
+begin
 if (@stock > @stockMinimo)
-end try
-begin catch
-error_message('Es necesario reponer')
-end catch;
+raiserror ('No es necesario realizar la reposición del stock',10,1)
+else
+raiserror ('realizar la reposición del stock',10,1)
+return 1
+end
 
 
 declare @s decimal(10,2)
 declare @sm decimal (10,2)
-declare @retorno char(50)
-exec @retorno= pa_consulta_repo_stock2 30 ,@s output, @sm output
+declare @retorno int
+exec @retorno= pa_consulta_repo_stock3 30 ,@s output, @sm output
 select  @s 'Stock', @sm 'Stock Minimo',@retorno 'Reposicion' 
 
 ---------------------------------------------------------------------
+/*Manejo de Errores*/
+
+-- Try.....Catch
+
+begin try 
+select 15/0 as error;
+end try
+begin catch
+select 'se produjo el siguiente error',
+error_number () as Número,
+error_state () as Estado,
+error_severity () as Gravedad,
+error_procedure () as Procedimiento,
+error_line () as Linea,
+error_message () as Mensaje
+end catch
+
+
+select * from sys.messages
+where message_id = 8134
+
+/*1. Modificar el ejercicio 2 de la sección 1.1 reemplazando los mensajes mostrados en
+consola con print, por excepciones. Verificar el comportamiento en el SQL Server
+Management.*/
+
+declare @codigo int=30, @stock int, @stockMinimo int
+select @stock=stock ,@stockminimo=stock_minimo
+	from articulos
+	where cod_articulo=@codigo
+if @stock is null or @stockMinimo is null 
+	raiserror ('hay valores nulos',11,1)
+else
+	if @stock<=@stockMinimo
+		raiserror ('realizar la reposición del stock',10,1)
+	else
+		raiserror ('No es necesario realizar la reposición del stock',10,1)
+
+
+
+
+/*2. Modificar el ejercicio anterior agregando las cláusulas de try catch para manejo de
+errores, y mostrar el mensaje capturado en la excepción con print.*/
+
+declare @codigo int=30, @stock int, @stockMinimo int
+select @stock=stock ,@stockminimo=stock_minimo
+	from articulos
+	where cod_articulo=@codigo
+if @stock is null or @stockMinimo is null 
+	raiserror ('hay valores nulos',11,1)
+else
+	begin try
+	if @stock<=@stockMinimo
+	print  'realizar la reposición del stock'
+	end try
+	begin catch
+	select ERROR_NUMBER() AS ErrorNumber,  
+          ERROR_MESSAGE() AS ErrorMessage;
+	end catch;
+	
+
+/******************************************************************************/
+
 
 /* Programacion aplicada a procediminetos y Funciones
 Ejercicio Procedimiento 1.a Mostrar los artículos cuyo precio sea mayor o igual que un valor que se envía por parámetro.*/
@@ -306,25 +367,170 @@ excepción que deberá ser manejada con try catch.
 */
 
 
-
-
 ALTER PROCEDURE pa_buscArticulo
 @descripcion nvarchar(500) = '%',
 @nombre nvarchar(500) output
 AS  
+BEGIN TRY
 begin
 select @nombre = descripcion from articulos where descripcion like @descripcion+'%'
 end      
-BEGIN TRY  
-    declare @n nvarchar(500)
-    EXECUTE pa_buscArticulo 'cuaderno', @n output
-	select @n 'descripcion'
 END TRY  
 BEGIN CATCH  
     SELECT   
         ERROR_NUMBER() AS ErrorNumber,  
-        ERROR_MESSAGE() AS ErrorMessage;  
+        ERROR_MESSAGE() AS ErrorMessage;
+		print 'Mas de un articulo'
 END CATCH;  
+
+declare @n nvarchar (500)
+EXECUTE pa_buscArticulo 'l%', @n output
+select @n 'descripcion'
+
+/* Programación de Funciones*/
+
+
+/*a. Devolver una cadena de caracteres compuesto por los siguientes datos:
+Apellido, Nombre, Telefono, Calle, Altura y Nombre del Barrio, de un
+determinado cliente, que se puede informar por codigo de cliente o email.*/
+
+alter function fn_Datos_Cliente
+(@parametro1 int = null, /* se recomienda pasar parametros por defecto*/
+@parametro2 nvarchar(100) = null)
+Returns nvarchar(100) 
+as
+begin
+declare @cadena nvarchar(500) 
+if (@parametro1 is not null and @parametro2 is null)
+     set @cadena = (select 'Nombre '+c.ape_cliente+','+c.nom_cliente+'Tel. '+c.nro_tel+'Calle '+c.calle+'N° '+c.altura+'B° '+b.barrio 
+     from clientes c inner join barrios b on c.cod_barrio = b.cod_barrio
+     where c.cod_cliente = @parametro1)
+else
+    if @parametro1 is null and @parametro2 is not null
+      set @cadena = (select 'Nombre '+c.ape_cliente+','+c.nom_cliente+'Tel. '+c.nro_tel+'Calle '+c.calle+'N° '+c.altura+'B° '+b.barrio
+      from clientes c inner join barrios b on c.cod_barrio = b.cod_barrio
+      where [e-mail] like @parametro2)
+    else
+	if @parametro1 is not null and @parametro2 is not null
+      set @cadena = (select 'Nombre '+c.ape_cliente+','+c.nom_cliente+'Tel. '+c.nro_tel+'Calle '+c.calle+'N° '+c.altura+'B° '+b.barrio
+      from clientes c inner join barrios b on c.cod_barrio = b.cod_barrio
+      where [e-mail] like @parametro2 and c.cod_cliente = @parametro1)
+    else
+	set @cadena= 'Debe ingresar un codigo de cliente o un mail' -- se puede usar el set y luego la consulta o se puede seleccionar y asigna como campo la consulta a la
+  return @cadena --el rerturn siempre hace referencia a un unicop SP y siempre esta dentro del begin.
+end;
+
+select dbo.fn_Datos_Cliente(1,default)
+
+
+
+/*b. Devolver todos los artículos, se envía un parámetro que permite ordenar el
+resultado por el campo precio de manera ascendente (‘A’), o descendente
+(‘D’).*/
+
+create function f_articulos2
+(@orden char(1) = null)
+returns table
+as
+if (@orden = 'A')
+return 
+(select * from articulos order by pre_unitario asc)
+else
+   if(@orden = 'D')
+   return 
+   (select * from articulos order by pre_unitario desc)
+   else
+   return 
+   (select 'debe ingresar A o D')
+
+
+select * f_articulos2
+
+--------------------------------------------------
+
+alter function f_art_ord_pre
+(@orden varchar(1) = null)
+returns @art_ord table
+(cod int, descrip varchar(100), pre money)
+as
+begin
+if (@orden = 'A')
+   insert @art_ord
+   select a.cod_articulo,a.descripcion,a.pre_unitario
+   from articulos a
+   order by a.pre_unitario
+ 
+else
+   if(@orden = 'D')
+     insert @art_ord
+     select a.cod_articulo,a.descripcion,a.pre_unitario
+     from articulos a
+     order by a.pre_unitario desc
+return
+end;
+
+select * from dbo.f_art_ord_pre('D')
+
+
+
+/*c. Crear una función que devuelva el precio al que quedaría un artículo en
+caso de aplicar un porcentaje de aumento pasado por parámetro.*/
+
+create function f_art_act_pre
+(@cod int,@por decimal(3,1))
+returns @pre_art table
+(cod int, descrip varchar(100),pre money)
+as 
+begin
+	insert @pre_art
+	select cod_articulo,descripcion,pre_unitario*(1+@por/100)
+	from articulos
+	where cod_articulo=@cod
+return
+end
+
+select * from dbo.f_art_act_pre(1,10)
+
+
+/**********************************************************************************************/
+
+/*TRIGGERS*/
+
+/*1. Crear un desencadenador para las siguientes acciones:
+a. Restar stock DESPUES de INSERTAR una VENTA*/
+/*b. Para no poder modificar el nombre de algún artículo
+create trigger dis_articulos_actualizar_nombre */
+
+ on articulos
+ for update
+ as
+ if update(descripcion)
+ begin
+ raiserror('No se puede modificar el nombre de un
+  artículo', 10, 1)
+ rollback transaction
+ end;
+
+
+/* c. Insertar en la tabla HistorialPrecio el precio anterior de un artículo si el
+mismo ha cambiado
+
+
+d. Bloquear al vendedor con código 4 para que no pueda registrar ventas en el
+sistema.*/
+
+create trigger dis_ventas_vendedor_4
+on facturas	
+for insert
+as
+declare @vendedor int
+select @vendedor=fa.cod_vendedor from facturas fa join inserted
+on inserted.cod_vendedor=fa.cod_vendedor
+if(@vendedor=4)
+begin
+raiserror('Error el vendedor 4 no puede cargar facturas', 10, 1)
+rollback transaction
+end;
 
  -- 2.5. Tablas Temporales
 
